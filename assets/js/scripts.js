c90bd54cb568fc3d5c9084f4f49e57255ed097f6
John-Yip-HK <yip4work@gmail.com>
2022-02-08 14:21:04 +0800

    Tooltip implementation
    Basic tooltip completed.
    Need to use d3.pointer to find out the coordinates of tooltip.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,11 @@
-const WIDTH = 700;
+const WIDTH = 800;
 const HEIGHT = 400;
 const PADDING = 50;
 
-const svg = d3
-  .select("div#container")
+const container = d3.select("div#container");
+container.append("h2").attr("id", "title").text("United States GDP");
+
+const svg = container
   .append("svg")
   .attr("id", "graph")
   .attr("width", WIDTH)
@@ -33,30 +35,57 @@ fetch(
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
+    const tooltip = container
+      .append("div")
+      .attr("id", "tooltip")
+      .html("Date<br>GDP");
+
+    function mouseover() {
+      const bar = d3.select(this);
+
+      tooltip
+        .style("display", "block")
+        .attr("data-date", bar.attr("data-date"))
+        .attr("data-gdp", bar.attr("data-gdp"));
+
+      bar.style("opacity", "0.4");
+    }
+
+    function mousemove(event) {
+      const date = tooltip.attr("data-date").split("-");
+      const gdp = tooltip.attr("data-gdp");
+
+      // const pointer = d3.pointer(event, container.node());
+      // console.log(pointer);
+      // const xCoor = pointer[0] - 2 * PADDING > 0 ? pointer[0] : PADDING * 2;
+      // const yCoor = pointer[1];
+
+      tooltip
+        .html(`${date[0]}-Q${quarterScale(date[1])}<br>$${gdp} Billion`)
+        .style("top", `${event.pageY}px`)
+        .style("left", `${event.pageX + 15}px`);
+    }
+
+    function mouseout() {
+      d3.select(this).style("opacity", "1");
+      tooltip.style("display", "none");
+    }
+
     svg
       .selectAll("rect")
       .data(gdp)
       .enter()
       .append("rect")
-      .attr("width", 2)
+      .attr("width", 2.5)
       .attr("height", (d) => valScale(d[1]))
       .attr("x", (_, i) => xScale(i))
       .attr("y", (d) => yScale(d[1]))
       .attr("data-date", (d) => d[0])
       .attr("data-gdp", (d) => d[1])
-      .attr("class", "bar");
-
-    const tooltip = d3
-      .select("div#container")
-      .append("div")
-      .style("position", "absolute")
-      .style("visibility", "hidden")
-      .text("I'm a bar!");
-
-    d3.select("#graph")
-      .on("mouseover", () => tooltip.style("visibility", "visible"))
-      .on("mousemove", () => tooltip.attr("x", 100).attr("y", 50))
-      .on("mouseout", () => tooltip.style("visibility", "hidden"));
+      .attr("class", "bar")
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseout", mouseout);
 
     svg
       .append("g")
